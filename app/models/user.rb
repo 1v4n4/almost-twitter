02002873101cd135d6244459f84380f2f46e7b5e
ivana <ivananl@yahoo.com>
2021-06-13 20:31:30 +0200

    Readme updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tvits
+  has_one_attached :avatar
   def show
   end
 end
